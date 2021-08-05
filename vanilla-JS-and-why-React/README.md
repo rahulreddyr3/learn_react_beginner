fedd33f1952dd3994ed2292f618df7b3f2dcfb75
Rahul <rahulreddyr3@gmail.com>
2021-08-05 19:52:28 +0530

    [Rahul] DOM manipulation with JS and CSS

--- vanilla-JS-and-why-React/README.md
+++ vanilla-JS-and-why-React/README.md
@@ -6,3 +6,8 @@
 
 ### Doing things JS way
 * To do thing in pure JS way, we need to create a html file and link our js file to it.
+
+### Let's build things
+* Now that the JS is linked to HTML, lets
+    * Add a few elements to HTML and change them from JS.
+    * Lets try to add CSS
--- vanilla-JS-and-why-React/app.js
+++ vanilla-JS-and-why-React/app.js
@@ -2,3 +2,18 @@
 
 // Test thing are working with the `secret hello`.
 console.log("Secret Hello");
+
+/////////////////////////// DOM Manipulation With Vanilla JS ///////////////////////////
+
+// To access the elements from html we can select them by the class name. (Note: There many other methods to do this. This felt easy)
+const title = document.querySelector(".title")
+
+// Once we have the element we can change it in JS and this will get reflected in the page.
+// title.style.color = "green";
+// title.style.backgroundColor = "red";
+
+// For JS we can also change the class of an element to apply styling.
+// For this create the class in a css file.
+// Link the css to Html with `<link rel="stylesheet" href="style.css"/>`
+
+// title.classList.add("change")
